982252c4afc5f6ab3c9c3a2cd442ee2261f48fab
leekwe <leekwe@163.com>
2023-10-10 16:03:48 +0800

    feat: 结婚日--UI搭建60%

---
+++ pages/marryDetail/chat/index.js
@@ -0,0 +1,33 @@
+import {
+	ref
+} from 'vue'
+export const chatItemList = ref([{
+	id: 1,
+	avatarUrl: 'http://192.168.20.221:3000/avatar/1696902854857.jpg',
+	content: '请问你中午吃的什么？',
+	userId: 2,
+	isMe: false,
+}, {
+	id: 2,
+	avatarUrl: 'http://192.168.20.221:3000/avatar/1696902854857.jpg',
+	content: '我吃的小鸡炖蘑菇？',
+	userId: 1,
+	isMe: true,
+}])
+
+export const content = ref('')
+
+
+export function addChatItem() {
+
+	console.log("####addChatItem");
+	if (content.value == '') return;
+	chatItemList.value.push({
+		id: chatItemList.value.length + 1,
+		content: content.value,
+		isMe: true,
+		avatarUrl: 'http://192.168.20.221:3000/avatar/1696902854857.jpg',
+		userId: 1
+	})
+	content.value = '';
+}
